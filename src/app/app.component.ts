43ed040337fc2e36430d97a992ad21d385ff4bf5
Mingzhe Li <mingzhe.helian@gmail.com>
2024-10-29 17:03:24 +1100

    feat(app): implement the calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {UserInputComponent} from "./user-input/user-input.component";
+import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
+import {InvestmentInput, InvestmentOutput} from "./investment-input.model";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [
+        HeaderComponent,
+        UserInputComponent,
+        InvestmentResultsComponent
+    ]
 })
-export class AppComponent {}
+export class AppComponent {
+    resultsData = signal<InvestmentOutput[] | undefined>(undefined);
+
+    onCalculateInvestmentResults = (data: InvestmentInput) => {
+        const {
+            initialInvestment,
+            duration,
+            expectedReturn,
+            annualInvestment
+        } = data;
+
+        const annualData = [];
+        let investmentValue = initialInvestment;
+
+        for (let i = 0; i < duration; i++) {
+            const year = i + 1;
+            const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+            investmentValue += interestEarnedInYear + annualInvestment;
+            const totalInterest =
+                investmentValue - annualInvestment * year - initialInvestment;
+            annualData.push({
+                year: year,
+                interest: interestEarnedInYear,
+                valueEndOfYear: investmentValue,
+                annualInvestment: annualInvestment,
+                totalInterest: totalInterest,
+                totalAmountInvested: initialInvestment + annualInvestment * year,
+            });
+        }
+
+        this.resultsData.set(annualData);
+    };
+
+}
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,16 @@
+import {Component, input} from '@angular/core';
+import {InvestmentInput, InvestmentOutput} from "../investment-input.model";
+import {CurrencyPipe} from "@angular/common";
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  results = input<InvestmentOutput[]>()
+}
